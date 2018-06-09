4177d414367b6aa27966e5408d01dd01f994f505
Mahendra <mahendratech70@gmail.com>
2018-06-09 22:38:33 +0530

    adding  multiple files together

---
+++ shopping-cart/online-shopping-cart/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Router from './Router'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1> My Shopping  cart</h1>
+
+          <Router />
+      </div>
+    );
+  }
+}
+
+export default App;
